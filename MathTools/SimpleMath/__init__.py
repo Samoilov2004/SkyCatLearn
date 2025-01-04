e3c47325b48f7c45215063f0d68a9dad0121b3c8
Samoilov2004 <samoilov.m1@yandex.ru>
2025-01-04 19:59:56 +0300

    добавлена документация и __init__.py файлы

---
+++ MathTools/SimpleMath/__init__.py
@@ -0,0 +1,6 @@
+# SkyCatLearn/MathTools/SimpleMath/__init__.py
+
+from .my_math import *
+
+__all__ = ['pi', 'e', 'tau', 'inf', 'nan', 'sin', 'cos', 'tan', 'asin', 'acos', 'atan', 'atan2', 'sinh', 'cosh', 'tanh', 'asinh', 'acosh', 'atanh', 'sqrt', 'log']
+
---
+++ MathTools/__init__.py
@@ -0,0 +1,6 @@
+# SkyCatLearn/MathTools/__init__.py
+
+from .vector import Vector
+from .SimpleMath import my_math
+
+__all__ = ['Vector', 'my_math']
